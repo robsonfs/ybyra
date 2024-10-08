30e941329ae4fc4189a0a655faad646e02e0f101
Robson Santos <robfds@gmail.com>
2024-10-08 01:22:52 -0300

    feat(Repository): WIP: exploring git2::Repository object

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use rocket::{get, launch, routes};
+use git2::Repository;
 
 #[get("/")]
 fn hello() -> String {
     let mut txt = String::from(
-        "Ybyrá: Strong, Open, and Decentralized—The Future of Code Sharing!\n"
+        "Ybyrá: Strong, Open, and Decentralized - The Future of Code Sharing!\n"
     );
 
     txt.push_str(
@@ -28,8 +29,27 @@ fn hello() -> String {
     txt
 }
 
+#[get("/")]
+fn repositories() -> String {
+    println!("I'm here");
+    match Repository::open("/home/robson/Projects/open_source/ybyra") {
+        Ok(repo) => format!("Repo Owner: {}",
+            repo
+                .config()
+                .unwrap()
+                .get_path("user.name")
+                .unwrap()
+                .to_str()
+                .unwrap()
+        ),
+        Err(e) => format!("Error: {}", e)
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+    rocket::build()
+        .mount("/", routes![hello])
+        .mount("/repositories", routes![repositories])
 }
 
